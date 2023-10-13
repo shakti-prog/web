868b07fa1fa0e732d974d7b7beee6f77a297d9b3
Shakti Tripathi <shakti.t@source.one>
2023-10-13 11:50:34 +0530

    Minor code change

--- service.js
+++ service.js
@@ -2,6 +2,7 @@
 
 
 export async function fetchSrData() {
+  console.log("Here in fethcing sr data");
   const response = await fetch("http://127.0.0.1:9000/getSrData");
   const { accepted, rejected, toDo, inProgress, done } = await response.json();
   return { accepted, rejected, toDo, inProgress, done };
