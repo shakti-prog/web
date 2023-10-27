a551c3bae2ad1d12c1662810ba4e9c1fafe2be96
Shakti Tripathi <shakti.t@source.one>
2023-10-27 16:41:51 +0530

    Minor changes in the code , made some changes in the UI

--- Components/components.js
+++ Components/components.js
@@ -545,7 +545,6 @@ class srForm extends HTMLElement {
           </div>
           <div class="mt-4 bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
             <button type="button" id="create-sr-button" class="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-700 sm:ml-3 sm:w-auto">Submit</button>
-            <button type="button" id="close-sr-form" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
           </div>
         </form>
       </div>
